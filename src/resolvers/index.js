d170977b3f5fd22f921fc5f2281263e886a3b673
Justin Yum <justinyum98@gmail.com>
2019-11-15 12:22:27 -0800

    fix

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,8 +1,7 @@
 module.exports = {
   Query: {
-    posts: async(_, __, { dataSources }) => {
-      const mediaObjects = await dataSources.instagramAPI.getAllMediaObjects();
-      return mediaObjects;
-    }
+    posts: (_, __, { dataSources }) => (
+      dataSources.instagramAPI.getAllMediaObjects()
+    )
   }
 }
