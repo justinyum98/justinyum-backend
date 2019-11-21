ae37751f6fafc1156c4723908f1c6772b1393988
Justin Yum <justinyum98@gmail.com>
2019-11-20 18:52:05 -0800

    added experience data, needs finishing

---
+++ src/data/experience.js
@@ -0,0 +1,22 @@
+const getAllExperience = () => ([
+  {
+    companyName: 'Playstation',
+    role: 'Engineering Intern',
+    imageUrl: 'https://picsum.photos/200',
+    date: 'June 2017 - Sept. 2017',
+    description: "In the summer of 2017, I scored my first job ever with Playstation. As an intern of the Accounts Service team, I developed Playstation’s Accounts API service in an Agile environment, tested that service with unit tests and integration tests, and fixed defects affecting our end-users. Thanks to my team, I felt more comfortable in expressing my ideas and suggestions, something that hasn’t changed. Overall, this opportunity widened my perspective on the software industry and broke me out of my shell. (Special thanks to Durga and Patrick for believing in me!)",
+    companyUrl: 'https://www.playstation.com/en-us/',
+  },
+  {
+    companyName: 'Dexcom',
+    role: 'Software Development Intern',
+    imageUrl: 'https://picsum.photos/200',
+    date: 'June 2018 - Sept. 2018',
+    description: "",
+    companyUrl: 'https://www.dexcom.com/',
+  }
+]);
+
+module.exports = {
+  getAllExperience
+};
