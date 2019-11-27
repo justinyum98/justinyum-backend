1cdb1bb942a1d0fe70e9478ce407ff0da6ef0655
Justin Yum <justinyum98@gmail.com>
2019-11-26 21:23:01 -0800

    deleted radius change

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ const getAllProjects = () => ([
   {
     name: 'This website!',
     role: 'Lead Developer',
-    imageUrl: cloudinary.url('justinyumwebsite_tcjkrc.png', {radius: 30, width: 300, crop: "scale"}),
+    imageUrl: cloudinary.url('justinyumwebsite_tcjkrc.png', { width: 300, crop: "scale" }),
     date: 'May 2019 - Present',
     description: 'The very website which you are viewing right now! Built with React and Material-UI, this website is an online portfolio that details my career achievements. It also showcases my abilities as a full-stack developer (both the frontend and backend code was entirely done by me).',
     repoUrl: 'https://github.com/justinyum98/justinyum-website',
