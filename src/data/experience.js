c6961260cba1c6bd466730414931502fc85cfc1b
Justin Yum <justinyum98@gmail.com>
2019-11-26 21:16:13 -0800

    updated image properties

--- src/data/experience.js
+++ src/data/experience.js
@@ -4,7 +4,7 @@ const getAllExperience = () => ([
   {
     companyName: 'Playstation',
     role: 'Engineering Intern',
-    imageUrl: cloudinary.url('playstationlogo_inphb2.png'),
+    imageUrl: cloudinary.url('playstationlogo_inphb2.png', {radius: 30, width: 300, crop: "scale"}),
     date: 'June 2017 - Sept. 2017',
     description: "In the summer of 2017, I scored my first job ever with Playstation. As an intern of the Accounts Service team, I developed Playstation’s Accounts API service in an Agile environment, tested that service with unit tests and integration tests, and fixed defects affecting our end-users. Thanks to my team, I felt more comfortable in expressing my ideas and suggestions, something that hasn’t changed. Overall, this opportunity widened my perspective on the software industry and broke me out of my shell. (Special thanks to Durga and Patrick for believing in me!)",
     companyUrl: 'https://www.playstation.com/en-us/',
@@ -12,7 +12,7 @@ const getAllExperience = () => ([
   {
     companyName: 'Dexcom',
     role: 'Software Development Intern',
-    imageUrl: cloudinary.url('dexcomlogo_rwctvv.jpg'),
+    imageUrl: cloudinary.url('dexcomlogo_rwctvv.jpg', {radius: 30, width: 300, crop: "scale"}),
     date: 'June 2018 - Sept. 2018',
     description: "Interning at Dexcom was a valuable learning experience that introduced me to the world of web development. In my time there, I learned HTML + CSS + Javascript and used Angular 6 to rebuild the user interface for my team's client token management system. Through that project, I gained a firm understanding of how HTTP requests work, how to use Promises, the importance of testing your UI components. (Thanks to Ranjit and Marco for taking time to help me.)",
     companyUrl: 'https://www.dexcom.com/',
