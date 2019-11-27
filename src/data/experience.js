5947982fad9929a381783e5562009ab027b6d760
Justin Yum <justinyum98@gmail.com>
2019-11-26 21:07:32 -0800

    added cloudinary for image hosting

--- src/data/experience.js
+++ src/data/experience.js
@@ -1,8 +1,10 @@
+const cloudinary = require('cloudinary').v2;
+
 const getAllExperience = () => ([
   {
     companyName: 'Playstation',
     role: 'Engineering Intern',
-    imageUrl: 'https://picsum.photos/200',
+    imageUrl: cloudinary.url('playstationlogo_inphb2.png'),
     date: 'June 2017 - Sept. 2017',
     description: "In the summer of 2017, I scored my first job ever with Playstation. As an intern of the Accounts Service team, I developed Playstation’s Accounts API service in an Agile environment, tested that service with unit tests and integration tests, and fixed defects affecting our end-users. Thanks to my team, I felt more comfortable in expressing my ideas and suggestions, something that hasn’t changed. Overall, this opportunity widened my perspective on the software industry and broke me out of my shell. (Special thanks to Durga and Patrick for believing in me!)",
     companyUrl: 'https://www.playstation.com/en-us/',
@@ -10,7 +12,7 @@ const getAllExperience = () => ([
   {
     companyName: 'Dexcom',
     role: 'Software Development Intern',
-    imageUrl: 'https://picsum.photos/200',
+    imageUrl: cloudinary.url('dexcomlogo_rwctvv.jpg'),
     date: 'June 2018 - Sept. 2018',
     description: "Interning at Dexcom was a valuable learning experience that introduced me to the world of web development. In my time there, I learned HTML + CSS + Javascript and used Angular 6 to rebuild the user interface for my team's client token management system. Through that project, I gained a firm understanding of how HTTP requests work, how to use Promises, the importance of testing your UI components. (Thanks to Ranjit and Marco for taking time to help me.)",
     companyUrl: 'https://www.dexcom.com/',
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,10 @@
+const cloudinary = require('cloudinary').v2;
+
 const getAllProjects = () => ([
   {
     name: 'This website!',
     role: 'Lead Developer',
-    imageUrl: 'https://picsum.photos/200',
+    imageUrl: cloudinary.url("justinyumwebsite_tcjkrc.png"),
     date: 'May 2019 - Present',
     description: 'The very website which you are viewing right now! Built with React and Material-UI, this website is an online portfolio that details my career achievements. It also showcases my abilities as a full-stack developer (both the frontend and backend code was entirely done by me).',
     repoUrl: 'https://github.com/justinyum98/justinyum-website',
